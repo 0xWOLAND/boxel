88282a38e550143ba9a7b52d26e27765e5400e2c
chndr1 <bhargav.annem@gmail.com>
2023-02-08 15:29:13 -0600

    rainbow triangle

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use std::iter;
 
+use wgpu::{ShaderModule, util::DeviceExt};
+
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
@@ -8,6 +10,40 @@ use winit::{
 
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+
+const VERTICES: &[Vertex] = &[
+    Vertex { position: [0.0, 0.5, 0.0], color: [1.0, 0.0, 0.0] },
+    Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
+    Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0] },
+];
+
+impl Vertex {
+    fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &[
+                wgpu::VertexAttribute {
+                    offset: 0,
+                    shader_location: 0,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+                wgpu::VertexAttribute {
+                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    shader_location: 1,
+                    format: wgpu::VertexFormat::Float32x3,
+                }
+            ]
+        }
+    }
+}
 
 struct State {
     surface: wgpu::Surface,
@@ -18,6 +54,9 @@ struct State {
     window: Window,
     clear_color: wgpu::Color,
     render_pipeline: wgpu::RenderPipeline,
+    shader: ShaderModule,
+    vertex_buffer: wgpu::Buffer,
+    num_vertices: u32
 }
 
 impl State {
@@ -29,6 +68,8 @@ impl State {
             b: 0.0, 
             a: 1.0
         };
+        let shader_string: &str = "shader.wgsl";
+        let num_vertices = VERTICES.len() as u32;
 
         // The instance is a handle to our GPU
         // BackendBit::PRIMARY => Vulkan + Metal + DX12 + Browser WebGPU
@@ -82,8 +123,16 @@ impl State {
             view_formats: vec![],
         };
         surface.configure(&device, &config);
-
+        
         let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
+        let vertex_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Vertex Buffer"),
+                contents: bytemuck::cast_slice(VERTICES),
+                usage: wgpu::BufferUsages::VERTEX,
+            }
+        );
+
 
         let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
             label: Some("Render Pipeline Layout"),
@@ -98,7 +147,7 @@ impl State {
                 vertex: wgpu::VertexState {
                     module: &shader,
                     entry_point: "vs_main",
-                    buffers: &[]
+                    buffers: &[Vertex::desc()]
                 },
                 fragment: Some(wgpu::FragmentState {
                     module: &shader,
@@ -135,7 +184,10 @@ impl State {
             size,
             window,
             clear_color,
-            render_pipeline
+            render_pipeline,
+            shader,
+            vertex_buffer,
+            num_vertices
         }
     }
 
@@ -197,13 +249,19 @@ impl State {
             });
 
             render_pass.set_pipeline(&self.render_pipeline);
-            render_pass.draw(0..3, 0..1);
+
+            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            render_pass.draw(0..self.num_vertices, 0..1);
         }
 
         self.queue.submit(iter::once(encoder.finish()));
         output.present();
 
         Ok(())
+    }
+
+    fn swap_shaders(&mut self) {
+        self.shader = self.device.create_shader_module(wgpu::include_wgsl!("shader_2.wgsl"));
     }
 }
 
@@ -256,11 +314,14 @@ pub async fn run() {
                             input:
                                 KeyboardInput {
                                     state: ElementState::Pressed,
-                                    virtual_keycode: Some(VirtualKeyCode::Escape),
+                                    virtual_keycode: Some(VirtualKeyCode::Space),
                                     ..
                                 },
                             ..
-                        } => *control_flow = ControlFlow::Exit,
+                        } => {
+                            
+                            state.swap_shaders();
+                        }
                         WindowEvent::Resized(physical_size) => {
                             state.resize(*physical_size);
                         }
